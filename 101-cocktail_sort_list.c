6781dc4dc8fc5d26375ad4f33b6eac3d9745d27e
Pelumi <pelumi.olalekan.g@gmail.com>
2022-11-08 03:20:42 +0000

    updated cocktail sort

--- 101-cocktail_sort_list.c
+++ 101-cocktail_sort_list.c
@@ -1,8 +1,10 @@
 #include "sort.h"
 #include <stdio.h>
 
-listint_t *shift_backward(listint_t *node, listint_t **dl_list);
-listint_t *shift_forward(listint_t *node);
+listint_t *shift_backward(listint_t *node,
+		listint_t **dl_list, listint_t **tail);
+listint_t *shift_forward(listint_t *node,
+		listint_t **dl_list, listint_t **tail);
 
 /**
  * cocktail_sort_list - a function that sorts a doubly linked list
@@ -15,13 +17,16 @@ listint_t *shift_forward(listint_t *node);
 
 void cocktail_sort_list(listint_t **list)
 {
-	listint_t *current, *head;
+	listint_t *current, *head, *tail;
 	int swapped;
 
 	current = head = *list;
 
 	if (list == NULL || head == NULL || head->next == NULL)
 		return;
+
+	for (tail = *list; tail->next != NULL;)
+		tail = tail->next;
 
 	while (1)
 	{
@@ -31,7 +36,7 @@ void cocktail_sort_list(listint_t **list)
 		{
 			if (current->next->n < current->n)
 			{
-				current = shift_forward(current);
+				current = shift_forward(current, list, &tail);
 				print_list(*list);
 				swapped = 1;
 			}
@@ -46,7 +51,7 @@ void cocktail_sort_list(listint_t **list)
 		{
 			if (current->prev->n > current->n)
 			{
-				current = shift_backward(current, list);
+				current = shift_backward(current, list, &tail);
 				print_list(*list);
 				swapped = 1;
 			}
@@ -64,24 +69,29 @@ void cocktail_sort_list(listint_t **list)
  * to the previous node
  * @node: the node of the list to swap
  * @dl_list: the whole doubly-linked list
+ * @tail: tail node of doubly linked list
  * Return: the list with the swapped nodes
  */
-listint_t *shift_backward(listint_t *node, listint_t **dl_list)
+listint_t *shift_backward(listint_t *node,
+		listint_t **dl_list, listint_t **tail)
 {
 	listint_t *previous_node = node->prev, *this_node = node;
 
-	previous_node->next = this_node->next;
 	if (this_node->next)
 		this_node->next->prev = previous_node;
+	else
+		*tail = previous_node;
 
+	previous_node->next = this_node->next;
 	this_node->prev = previous_node->prev;
-	this_node->next = previous_node;
-	previous_node->prev = this_node;
 
-	if (this_node->prev)
-		this_node->prev->next = this_node;
+	if (previous_node->prev)
+		previous_node->prev->next = this_node;
 	else
 		*dl_list = this_node;
+
+	this_node->next = previous_node;
+	previous_node->prev = this_node;
 
 	return (this_node);
 }
@@ -90,22 +100,30 @@ listint_t *shift_backward(listint_t *node, listint_t **dl_list)
  * shift_forward - swap the node of a doubly-linked list
  * to the next node
  * @node: the node of the list to swap
+ * @dl_list: head node of doubly linked list
+ * @tail: tail node of doubly linked list
  * Return: the list with the swapped nodes
  */
-listint_t *shift_forward(listint_t *node)
+listint_t *shift_forward(listint_t *node,
+		listint_t **dl_list, listint_t **tail)
 {
 	listint_t *next_node = node->next, *this_node = node;
 
-	next_node->prev = this_node->prev;
 	if (this_node->prev)
 		this_node->prev->next = next_node;
+	else
+		*dl_list = next_node;
 
+	next_node->prev = this_node->prev;
 	this_node->next = next_node->next;
+
+	if (next_node->next)
+		next_node->next->prev = this_node;
+	else
+		*tail = this_node;
+
 	this_node->prev = next_node;
 	next_node->next = this_node;
-
-	if (this_node->next)
-		this_node->next->prev = this_node;
 
 	return (this_node);
 }
